048d5695833f42c42b560286527f64f37c31a6e9
Рафаэль <rafael@MacBook-Air-Rafael.local>
2023-01-02 03:58:48 +0300

    day07 ex01 added

---
+++ day07/src/ex01/Annotations/Processor/src/main/java/edu/school21/reflection/annotations/HtmlInput.java
@@ -0,0 +1,14 @@
+package edu.school21.reflection.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface HtmlInput {
+    String type();
+    String name();
+    String placeholder();
+}
---
+++ day07/src/ex01/Annotations/User/src/main/java/edu/school21/reflection/forms/UserForm.java
@@ -0,0 +1,18 @@
+package edu.school21.reflection.forms;
+
+import edu.school21.reflection.annotations.HtmlForm;
+import edu.school21.reflection.annotations.HtmlInput;
+
+@HtmlForm(fileName = "user_form.html", action = "/users", method = "post")
+public class UserForm {
+
+    @HtmlInput(type = "text", name = "first_name", placeholder = "Enter First Name")
+    private String firstName;
+
+    @HtmlInput(type = "text", name = "last_name", placeholder = "Enter Last Name")
+    private String lastName;
+
+    @HtmlInput(type = "password", name = "password", placeholder = "Enter Password")
+    private String password;
+
+}
