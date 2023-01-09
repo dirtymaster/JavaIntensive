ff393e54f193962753b427b1a61c41e6ac825f34
Rafael Kunakov <91501721+abceff@users.noreply.github.com>
2023-01-09 18:55:01 +0300

    Delete Creating library archives and using external libraries directory

--- Creating library archives and using external libraries/src/ex00/ImagesToChar/src/java/edu/school21/printer/app/Program.java
+++
@@ -1,9 +0,0 @@
-package edu.school21.printer.app;
-
-import edu.school21.printer.logic.Logic;
-
-public class Program {
-    public static void main(String[] args) {
-        new Logic(args).run();
-    }
-}
